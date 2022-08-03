fb64e38635f47ef1ac6b46508e11f3b6308db02f
Kaliabianau, Vitali <vitali.kaliabianau@ihsmarkit.com>
2022-08-03 19:50:16 +0300

    Remove AggressiveInlining

--- KeyValueDb.Paging/PageIndex.cs
+++ KeyValueDb.Paging/PageIndex.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
 namespace KeyValueDb.Paging;
 
@@ -15,39 +14,27 @@ public readonly struct PageIndex : IEquatable<PageIndex>
 		Value = value;
 	}
 
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static implicit operator PageIndex(uint value) => new(value);
 
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public bool Equals(PageIndex other) => Value == other.Value;
 
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public override bool Equals(object? obj) => obj is PageIndex other && Equals(other);
 
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public override int GetHashCode() => Value.GetHashCode();
 
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static bool operator ==(PageIndex left, PageIndex right) => left.Equals(right);
 
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static bool operator !=(PageIndex left, PageIndex right) => !left.Equals(right);
 
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static bool operator >(PageIndex left, PageIndex right) => left.Value > right.Value;
 
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static bool operator >=(PageIndex left, PageIndex right) => left.Value >= right.Value;
 
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static bool operator <(PageIndex left, PageIndex right) => left.Value < right.Value;
 
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static bool operator <=(PageIndex left, PageIndex right) => left.Value <= right.Value;
 
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static PageIndex operator +(PageIndex left, int right) => (uint)(left.Value + right);
 
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static PageIndex operator ++(PageIndex left) => left.Value + 1;
 }

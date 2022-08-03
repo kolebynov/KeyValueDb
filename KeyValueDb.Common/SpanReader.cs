645a491c8736f7e5046bade2e877c49b75515a54
Kaliabianau, Vitali <vitali.kaliabianau@ihsmarkit.com>
2022-08-03 18:45:17 +0300

    Add missed files

---
+++ KeyValueDb.Common/SpanReader.cs
@@ -0,0 +1,26 @@
+﻿namespace KeyValueDb.Common;
+
+public ref struct SpanReader<T>
+{
+	private readonly ReadOnlySpan<T> _span;
+	private int _currentIndex;
+
+	public SpanReader(ReadOnlySpan<T> span)
+	{
+		_span = span;
+		_currentIndex = 0;
+	}
+
+	public ReadOnlySpan<T> Read(int length)
+	{
+		if (_currentIndex + length > _span.Length)
+		{
+			throw new ArgumentException("Length too big", nameof(length));
+		}
+
+		var result = _span.Slice(_currentIndex, length);
+		_currentIndex += length;
+
+		return result;
+	}
+}
---
+++ KeyValueDb.Common/SpanWriter.cs
@@ -0,0 +1,24 @@
+﻿namespace KeyValueDb.Common;
+
+public ref struct SpanWriter<T>
+{
+	private readonly Span<T> _span;
+	private int _currentIndex;
+
+	public SpanWriter(Span<T> span)
+	{
+		_span = span;
+		_currentIndex = 0;
+	}
+
+	public void Write(ReadOnlySpan<T> data)
+	{
+		if (_currentIndex + data.Length > _span.Length)
+		{
+			throw new ArgumentException("Data length too big", nameof(data));
+		}
+
+		data.CopyTo(_span[_currentIndex..]);
+		_currentIndex += data.Length;
+	}
+}

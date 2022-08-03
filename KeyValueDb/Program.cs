f7051c9a634dfd6595e23e9c6815dea0008274f5
Kaliabianau, Vitali <vitali.kaliabianau@ihsmarkit.com>
2022-08-03 19:05:09 +0300

    Program.cs

--- KeyValueDb/Program.cs
+++ KeyValueDb/Program.cs
@@ -1,32 +1,47 @@
-﻿using System.Globalization;
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Globalization;
 using System.Text;
 using KeyValueDb;
 
-var smallString = GetString(10);
+foreach (DictionaryEntry entry in Environment.GetEnvironmentVariables().Cast<DictionaryEntry>().OrderBy(x => x.Key))
+{
+	Console.WriteLine($"{entry.Key}={entry.Value}");
+}
+
+var timer = Stopwatch.StartNew();
+
+var smallString = GetString(38);
 var mediumString = GetString(100);
-var largeString = GetString(1000);
+var largeString = GetString(200);
+
+var strings = new[] { smallString, mediumString, largeString, smallString, mediumString };
+
+Console.WriteLine($"Strings allocated: {timer.Elapsed}");
 
 using var db = new Database("test.db");
 db.Set("key1", smallString);
 db.Set("key2", mediumString);
 db.Set("key3", largeString);
-db.Set("key4", largeString);
-db.Set("key5", largeString);
-db.Set("key6", largeString);
-db.Set("key7", largeString);
-db.Set("key8", largeString);
-db.Set("key9", largeString);
-db.Set("key10", largeString);
-db.Set("key11", largeString);
-db.Set("key12", largeString);
-db.Set("key13", largeString[..710]);
+var buffer = new byte[200];
 
-foreach (var key in new[] { "key1", "key2", "key3", "key4", "key5", "key6" })
+Console.WriteLine($"Strings stored: {timer.Elapsed}");
+var iterations = 1_000_000;
+
+for (var i = 0; i < iterations; i++)
 {
-	Console.WriteLine($"{key}: {Encoding.UTF8.GetString(db.Get(key)!)}");
+	db.TryGet("key1", buffer);
+	db.TryGet("key2", buffer);
+	db.TryGet("key3", buffer);
 }
 
-#pragma warning disable CS8321
+Console.WriteLine($"Strings read: {timer.Elapsed}, iterations: {iterations}");
+Console.WriteLine("Result strings:");
+
+foreach (var s in new[] { "key1", "key2", "key3" })
+{
+	Console.WriteLine(Encoding.ASCII.GetString(db.Get(s)!));
+}
+
 static byte[] GetString(int length) =>
-#pragma warning restore CS8321
 	Encoding.ASCII.GetBytes(string.Join(string.Empty, Enumerable.Range(0, length).Select(x => (x % 10).ToString(CultureInfo.InvariantCulture))));

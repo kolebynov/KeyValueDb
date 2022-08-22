2dd9e31e0e2630dfc737ff11b9103917bf723aa8
Kaliabianau, Vitali <vitali.kaliabianau@ihsmarkit.com>
2022-08-22 21:46:51 +0300

    fix: Add initial benchmarks

---
+++ KeyValueDb.Benchmarks/DatabaseBenchmarks.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace KeyValueDb.Benchmarks;
+
+#pragma warning disable CA1001
+public class DatabaseBenchmarks
+#pragma warning restore CA1001
+{
+	private readonly byte[] _buffer = new byte[200];
+	private Database _database = null!;
+
+	[GlobalSetup]
+	public void Setup()
+	{
+		_database = new Database("benchmark.db");
+		_database.Set("key", GetString(38));
+	}
+
+	[Benchmark]
+	public void TryGetBenchmark()
+	{
+		_database.TryGet("key1", _buffer);
+	}
+
+	[GlobalCleanup]
+	public void Cleanup()
+	{
+		_database.Dispose();
+	}
+
+	private static byte[] GetString(int length) =>
+		Encoding.ASCII.GetBytes(string.Join(
+			string.Empty,
+			Enumerable.Range(0, length).Select(x => (x % 10).ToString(CultureInfo.InvariantCulture))));
+}
---
+++ KeyValueDb.Benchmarks/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using KeyValueDb.Benchmarks;
+
+BenchmarkRunner.Run<DatabaseBenchmarks>(DefaultConfig.Instance
+	.AddJob(Job.Default
+		.WithRuntime(CoreRuntime.Core60)
+		.WithEnvironmentVariables(
+			new EnvironmentVariable("DOTNET_TC_QuickJitForLoops", "1"),
+			new EnvironmentVariable("DOTNET_TieredPGO", "1"),
+			new EnvironmentVariable("DOTNET_ReadyToRun", "0")))
+	.AddLogger(ConsoleLogger.Default));
